eeb346f675afdf471b7926530e7de3aa00dfb094
root <root@wheezy.localhost>
2013-07-20 00:56:54 -0400

    hidding the login panel by default and making it slides when the signin button is clicked

--- app.js
+++ app.js
@@ -9,24 +9,21 @@ Ext.application({
 	
 	
     launch: function() {
-		var log= Ext.create('Ext.container.Viewport', {
-          
+		var log = Ext.create('Ext.container.Viewport', {
+          	id:'mainPortal',
             items: [
             {
-                xtype: 'loginview',
-                id:'logview',
-                         
-            }
-			],
-			
-       
+                xtype:'loginview',
+                id:'logview'
+            }],
         });
 			log.items.first().setPosition(898,168);
 			$(document).ready(function(){
 			$("#textButton").click(function(){
-					Ext.getCmp("inscrit").hide();
-
-				//Ext.get("logview").slideOut();
+				var portal = Ext.getCmp("mainPortal");
+				//removing the css class that hides this login form and sliding the panel down
+				portal.down('form').removeCls('hideForm');
+				Ext.get('logview').slideIn();
 			});
 			});
 			
--- app/view/login/login.js
+++ app/view/login/login.js
@@ -1,46 +1,41 @@
 Ext.define('CP.view.login.login',{
 			extend:'Ext.form.Panel',
 			alias:'widget.loginview',
+			id:'loginForm',
 			title:'Login',
 			width:300,
             height: 160,
+			//add a css class to hide the form by default. Here we cannot use "hidden:true", because we lose the box style after.
+			cls:'hideForm',
 			frame:'true',
-			//hiden:true,
-			cls: 'round-corners',
-			id:'inscrit',
-			listeners:{
-				'afterrender':function(){
-					console.log(this.title);
-					}
-				
-				},
 			items:[
 				{
-				xtype:'textfield',
-				fieldLabel:'Username',
-				margin: '10 20 10 0',
-				name:'username',
-				itemId: 'username', 
-				allowblank: false,
-				},
-				{
-				 xtype: 'textfield',
-				 fieldLabel: 'Password',
-				 margin: '0 30 0 0',
-				 name: 'password', 
-				 itemId: 'password', 
-				 allowblank: false, 
-				 inputType: 'password',	
-				 
-				}
-			],
+					xtype:'textfield',
+					fieldLabel:'Username',
+					margin: '10 20 10 0',
+					name:'username',
+					itemId: 'username', 
+					allowblank: false,
+				},{
+					 xtype: 'textfield',
+					 fieldLabel: 'Password',
+					 margin: '0 30 0 0',
+					 name: 'password', 
+					 itemId: 'password', 
+					 allowblank: false, 
+					 inputType: 'password',	 
+				}],
+			listeners:{
+					//sliding the login form up when rendering it. The same thing is possible with 'afterrender' event
+					'render': function(){
+						Ext.get('logview').slideOut();
+					}
+			},
 			buttons:[{
-			text:'Sign In',
-			action:'connect',
-			margin: '5 5 5 5',
-
-			
-				/*listeners :{ 
+				text:'Sign In',
+				action:'connect',
+				margin: '5 5 5 5',	
+				/*listeners :{
 					click: function(){ 
 						var username = this.up('form').down('#username').getValue(); 
 						var password = this.up('form').down('#password').getValue(); 
